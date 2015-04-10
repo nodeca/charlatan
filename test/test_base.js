f8e5908653b1dc5eb4d292a05ef5cac944bfb91d
Alex Kocharin <alex@kocharin.ru>
2015-04-10 11:15:27 +0000

    Add regexify + Finance

---
+++ test/test_base.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/* globals it, describe */
+
+require('./_utils');
+
+var assert = require('assert');
+var faker = require('../');
+
+describe('Base', function () {
+  it('numerify', function () {
+    for (var i = 0; i < 100; i++) {
+      assert(faker.numerify('###').match(/[1-9]\d{2}/));
+    }
+  });
+
+  it('letterify', function () {
+    assert(faker.letterify('???').match(/[A-Z]{3}/));
+  });
+
+  it('regexify', function () {
+    var dict = {
+      'uk post code': /^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$/,
+      'us phone': /^(1-?)[2-8][0-1][0-9]-\d{3}-\d{4}$/
+    };
+
+    Object.keys(dict).forEach(function (label) {
+      var result;
+
+      for (var i = 0; i < 10; i++) {
+        result = faker.regexify(dict[label]);
+        assert(dict[label].test(result), result + 'is not a match for ' + label);
+      }
+    });
+  });
+});
---
+++ test/test_finance.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/* globals it, describe, before */
+
+require('./_utils');
+
+var assert = require('assert');
+var faker = require('../');
+
+describe('finance', function () {
+  var valid_ones = [
+    '3019-872113-1171',
+    '3051-346334-7720',
+    '5019-8865-5667-8258',
+    '506520872154623929',
+    '5213-8618-8032-6704',
+    '5420-2970-4793-7043',
+    '6011-6282-1135-7355-7809',
+    '6461-6239-1512-8072-8338',
+    '6709999639407938958',
+    '6767-2952-1470-2154-617'
+  ];
+
+  before(function () {
+    var obj = {};
+
+    valid_ones.forEach(function (x) {
+      obj[x] = x.replace(/\d$/, 'L');
+    });
+
+    faker.addLocale('credit_cards', {
+      credit_card: obj
+    });
+
+    faker.setLocale('credit_cards');
+  });
+
+  it('credit_card should compute last digit', function () {
+    valid_ones.forEach(function (x) {
+      assert.equal(faker.Finance.creditCard(x), x);
+    });
+  });
+});
